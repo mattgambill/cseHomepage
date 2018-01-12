d96c3862a83981155e2365198c792a5c9f35b72a
Matt Gambill <mattgambill123@gmail.com>
2018-01-12 02:03:38 -0500

    deleted:    go.sh

--- go.sh
+++
@@ -1,7 +0,0 @@
-echo "# cseHomepage" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/mattgambill/cseHomepage.git
-git push -u origin master
-
